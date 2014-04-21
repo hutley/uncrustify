a836674da471d6811812fcd941b8c9dba1c40ccd
Aaron Blohowiak <ablohowiak@fanhattan.com>
2014-04-21 11:00:18 -0700

    new example round robins

--- load-routes.rb
+++ load-routes.rb
@@ -12,9 +12,10 @@ def load_yaml(name)
 end
 
 backends = load_yaml('backends')[env]
+round_robins = load_yaml('round_robins')[env]
 routes = load_yaml('routes')
 
-backends_by_id = backends.inject({}){|memo, backend| memo[backend['id']] = backend; memo }
+backends_by_id = [backends|round_robins].flatten.inject({}){|memo, backend| memo[backend['id']] = backend; memo }
 
 routes.each do |route|
   if backends_by_id[route['backend_id']].nil?
@@ -30,18 +31,25 @@ def submit(subpath, data)
   @http.put(@router_api_url + subpath, JSON.dump(data), 'Content-Type'=>'application/json')
 end
 
-backends.each do |backend|
-  response = submit('/backends/'+backend['id'], backend)
-  if !response.code.start_with?('20')
-    $stderr.puts "Problem upserting backend. #{backend}.\n #{response.inspect}"
-    exit -1
+all_backends = {
+  '/backends/' => backends,
+  '/round_robins/' => round_robins
+}
+
+all_backends.each_pair do |url, backends|
+  backends.each do |backend|
+    response = submit(url+backend['id'], backend: backend)
+    if !response.code.start_with?('20')
+      $stderr.puts "Problem upserting backend. #{backend}.\n #{response.inspect} \n #{response.body rescue nil}"
+      exit -1
+    end
   end
 end
 
 routes.each do |route|
   response = submit('/routes', route)
   if !response.code.start_with?('20')
-    $stderr.puts "Problem upserting route. #{route}.\n #{response.inspect}"
+    $stderr.puts "Problem upserting route. #{route}.\n #{response.inspect} \n #{response.body rescue nil}"
     exit -1
   end
 end
